7c4ff84b8cc52cec29960e1aa77253f81e47fbb8
sambitkumarjethy <sambitkumarjethy@gmail.com>
2025-01-07 09:06:26 +0530

    app routing setup and logo added

---
+++ namste_react/netflix-gpt/src/components/Login.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Netflix_Logo_RGB from "../assets/Netflix_Logo_RGB.png";
+
+const Login = () => {
+  return (
+    <div>
+      <img
+        src={Netflix_Logo_RGB}
+        alt="Netflix_Logo_RGB"
+        style={{ width: "200px", height: "auto" }}
+      />
+    </div>
+  );
+};
+
+export default Login;
