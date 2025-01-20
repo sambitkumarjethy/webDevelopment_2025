562ee4ccd28d52fab809afc6f12016fa51f2ee57
sambitkumarjethy <sambitkumarjethy@gmail.com>
2025-01-20 21:09:29 +0530

    login page UI

--- namste_react/netflix-gpt/src/components/Login.js
+++ namste_react/netflix-gpt/src/components/Login.js
@@ -1,7 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./Header";
 
 const Login = () => {
+  const [isSignInForm, setIsSignInForm] = useState(true);
+  const toggleSignupForm = () => {
+    setIsSignInForm(!isSignInForm);
+  };
   return (
     <div>
       <Header />
@@ -16,7 +20,17 @@ const Login = () => {
       </div>
 
       <form className="w-3/12 absolute p-12 bg-black my-36 mx-auto right-0 left-0 text-white rounded-lg bg-opacity-80">
-        <h1 className="font-bold text-3xl py-4 ">Sign In</h1>
+        <h1 className="font-bold text-3xl py-4 ">
+          {isSignInForm ? "Sign In" : "Sign Up"}
+        </h1>
+        {!isSignInForm && (
+          <input
+            type="text"
+            placeholder="Full Name"
+            className="p-4 my-4 w-full bg-gray-700 bg-opacity-30"
+          />
+        )}
+
         <input
           type="text"
           placeholder="Email Address"
@@ -28,9 +42,16 @@ const Login = () => {
           className="p-4 my-4 w-full bg-gray-700 bg-opacity-30"
         />
         <button className="p-4 my-6 bg-red-700 w-full rounded-lg">
-          Sign In
+          {isSignInForm ? "Sign In" : "Sign Up"}
         </button>
-        <p>New to Netflix ? Signup Now </p>
+        <p
+          className="py-4 cursor-pointer hover:text-red-600"
+          onClick={toggleSignupForm}
+        >
+          {isSignInForm
+            ? "  New to Netflix ? Signup Now"
+            : "Already registed ? SignIn Now"}
+        </p>
       </form>
     </div>
   );
