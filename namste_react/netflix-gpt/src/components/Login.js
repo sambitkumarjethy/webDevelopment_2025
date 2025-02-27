00f5ec1e0da3d263571d314856a6abf07c89f5e0
sambitkumarjethy <sambitkumarjethy@gmail.com>
2025-02-27 20:49:34 +0530

    user signIn functionality

--- namste_react/netflix-gpt/src/components/Login.js
+++ namste_react/netflix-gpt/src/components/Login.js
@@ -1,7 +1,10 @@
 import React, { useRef, useState } from "react";
 import Header from "./Header";
 import { checkValidData } from "../utils/validate.js";
-import { createUserWithEmailAndPassword } from "firebase/auth";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
 import { auth } from "../utils/firebase.js";
 
 const Login = () => {
@@ -47,6 +50,22 @@ const Login = () => {
         });
     } else {
       //sign In Logic
+      signInWithEmailAndPassword(
+        auth,
+        email.current.value,
+        password.current.value
+      )
+        .then((userCredential) => {
+          // Signed in
+          const user = userCredential.user;
+          console.log(user);
+        })
+        .catch((error) => {
+          console.log(error.message);
+          const errorCode = error.code;
+          const errorMessage = error.message;
+          setErrorMsg(errorCode + "-" + errorMessage);
+        });
     }
   };
   return (
