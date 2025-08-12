4d726bfdab60cf484041005ed4201230f0fade03
sambitkumarjethy <sambitkumarjethy@gmail.com>
2025-08-12 21:23:00 +0530

    codingshuttle - 12.08.2025- Setting up springboot project

---
+++ java/coding_shuttle/week1/introductionToSpringBoot/src/test/java/com/sambitjethy/week1/introduction/introductionToSpringBoot/IntroductionToSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sambitjethy.week1.introduction.introductionToSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntroductionToSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
