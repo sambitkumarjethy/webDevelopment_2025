077e11e6eecc0fb5e622b575175cfb8030b39b1f
sambitkumarjethy <sambitkumarjethy@gmail.com>
2025-11-20 08:26:27 +0530

    Lambda & streams

--- java/coding_shuttle/week2/coding/mvnrestapi/src/main/java/com/sambit/week2mvn/mvnrestapi/MvnrestapiApplication.java
+++ java/coding_shuttle/week2/coding/mvnrestapi/src/main/java/com/sambit/week2mvn/mvnrestapi/MvnrestapiApplication.java
@@ -4,6 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 @SpringBootApplication
@@ -18,11 +21,47 @@ public class MvnrestapiApplication {
 		};
 		obj.walk(10);
 
-		List<String> fruits = List.of("Apple","Banana","Kiwi");
+		List<String> fruits = List.of("Apple","Banana","Kiwi","Avocado","Mango");
 		Stream<String> stream = fruits.stream();
-		stream.forEach((fruit) -> {
-			System.out.println(fruit);
-		});
+//		stream.forEach((fruit) -> {
+//			System.out.println(fruit);
+//		});
+
+//		stream
+//				.filter(fruit -> fruit.length() < 5)
+//				.sorted()
+//				.map(fruit ->fruit.length())
+//				.map(fruitlengeth -> 2*fruitlengeth)
+//				.forEach(fruit -> System.out.println(fruit));
+
+
+//		 Set<Integer> fruitsList =  fruits
+//				.stream()
+//				.map(fruit -> fruit.length())
+//				.collect(Collectors.toSet());
+
+//		Map<Integer , Integer> fruitsList =  fruits
+//				.stream()
+//				.map(fruit -> fruit.length())
+//						.collect(Collectors.toMap(
+//								key->key,
+//								value->value
+//						));
+
+		Map<String , Integer> fruitsList =  fruits
+				.stream()
+						.collect(Collectors.toMap(
+								fruit -> fruit,
+                                String::length
+						));
+
+
+
+
+
+
+		 System.out.println(fruits);
+		System.out.println(fruitsList);
 	}
 
 }
